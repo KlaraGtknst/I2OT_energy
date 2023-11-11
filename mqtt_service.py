b7216ae2a7be8d28702ae960d1b9a93b2f57f66b
Liphip <philip.laskowicz@web.de>
2023-11-11 08:34:13 +0100

    Add InfluxDB Client

---
+++ mqtt_service.py
@@ -0,0 +1,20 @@
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "I2OT_energy"
+url = "http://127.0.0.1:8086"
+bucket="energydata"
+
+write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
+   
+for value in range(5):
+  point = (
+    Point("measurement1")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", value)
+  )
+  write_api.write(bucket=bucket, org="I2OT_energy", record=point)
+  time.sleep(1) # separate points by 1 second
