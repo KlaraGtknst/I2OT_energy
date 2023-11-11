8c563312d4fbc5f3b6551bbca5b451502a00c59c
Liphip <philip.laskowicz@web.de>
2023-11-11 11:04:18 +0100

    Add dynamic tagging

--- constants.py
+++ constants.py
@@ -14,26 +14,51 @@ BUCKET="energydata"
 
 # MQTT Topics
 MQTT_TOPICS = {
-    "meters": {
-        "meter001": "shellyem3-C8C9A3705CFC",
-        "meter002": "shellyem3-E89F6D84968A",
-        "meter003": "SMA 015D.1901407994",
-        "meter004": "SMA 0174.3017198275",
+    # Zaehler
+    "meter": {
+        "meter_001": "shellyem3-C8C9A3705CFC",
+        "meter_002": "shellyem3-E89F6D84968A",
+        "meter_003": "SMA_015D.1901407994",
+        "meter_004": "SMA_0174.3017198275",
     },
-    "inverters": {
-        "inverter001": "SMA 017A.3017445303",
-        "inverter002": "SMA 01B8.3015653785"
+    # Weschelrichter
+    "inverter": {
+        "inverter_001": "SMA_017A.3017445303",
+        "inverter_002": "SMA_01B8.3015653785"
     }
 }
 
 MQTT_SUBTOPICS = {
-    "meters": {
+    "meter": {
         "power": "TotalActivePower"
     },
-    "inverters": {
+    "inverter": {
         "power": "TotalActivePower",
         "string_a": "String A",
         "string_b": "String B",
         "battery": "Battery",
     }
+}
+
+# InfluxDB Tags
+d_swap_list = [{value: key for key, value in MQTT_TOPICS[device].items()} for device in MQTT_TOPICS.keys()]
+TOPIC_LOOKUP = dict(pair for d in d_swap_list for pair in d.items())
+del d_swap_list
+
+DEVICE_LOOKUP = {
+    "meter_001": "buero",
+    "meter_002": "wohnung01",
+    "meter_003": "keller",
+    "meter_004": "hausanschluss",
+    "inverter_001": "sunnytripower4",
+    "inverter_002": "sunnytripower10se",
+}
+
+DEVICE_TYPE = {
+    "meter_001": "meter",
+    "meter_002": "meter",
+    "meter_003": "meter",
+    "meter_004": "meter",
+    "inverter_001": "inverter",
+    "inverter_002": "inverter",
 }
--- mqtt_service.py
+++ mqtt_service.py
@@ -3,6 +3,7 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from paho.mqtt import client as mqtt_client
 from constants import *
+import json
 
 
 def connect_mqtt():
@@ -17,25 +18,35 @@ def connect_mqtt():
     client.connect(BROKER, PORT)
     return client
 
+def wirte_data(device_topic, payload):
+    device_id = TOPIC_LOOKUP[device_topic]
+    device_type = DEVICE_TYPE[device_id]
+    device_name = DEVICE_LOOKUP[device_id]
+
+    print(f"Device: {device_id} - {device_type} - {device_name}")
+    print(f"Payload: {payload} - {type(payload)}")
+
+
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        wirte_data( msg.topic.split('/')[1], json.loads(msg.payload.decode()))
     
     def subscribe_to_topic(client:mqtt_client, main_topic, sub_topic):
         topic = f"{BASETOPIC}/{main_topic}/{sub_topic}"
         client.subscribe(topic)
         client.on_message = on_message
 
-    for meter in MQTT_TOPICS["meters"]:
-        meter_topic = MQTT_TOPICS["meters"][meter]
-        for subtopic_name in MQTT_SUBTOPICS["meters"]:
-            subtopic = MQTT_SUBTOPICS["meters"][subtopic_name]
+    for meter in MQTT_TOPICS["meter"]:
+        meter_topic = MQTT_TOPICS["meter"][meter]
+        for subtopic_name in MQTT_SUBTOPICS["meter"]:
+            subtopic = MQTT_SUBTOPICS["meter"][subtopic_name]
             subscribe_to_topic(client, meter_topic, subtopic)
     
-    for inverter in MQTT_TOPICS["inverters"]:
-        inverter_topic = MQTT_TOPICS["inverters"][inverter]
-        for subtopic_name in MQTT_SUBTOPICS["inverters"]:
-            subtopic = MQTT_SUBTOPICS["inverters"][subtopic_name]
+    for inverter in MQTT_TOPICS["inverter"]:
+        inverter_topic = MQTT_TOPICS["inverter"][inverter]
+        for subtopic_name in MQTT_SUBTOPICS["inverter"]:
+            subtopic = MQTT_SUBTOPICS["inverter"][subtopic_name]
             subscribe_to_topic(client, inverter_topic, subtopic)
 
 def connect_influxdb():
