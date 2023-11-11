4948c468a073f52e85dcedb724d1bc7fb82e46ba
Aleyna Albayrak <uk076901@student.uni-kassel.de>
2023-11-11 17:15:18 +0100

    feat: lstm

--- prognose/data/energy_data.py
+++ prognose/data/energy_data.py
@@ -13,13 +13,24 @@ class EnergyDataset(Dataset):
         self.data = pd.read_csv(path)
         self.data = self.data[['_value']]
         self.window_size = window_size
-        pass
+
+        self.X, self.y = [], []
+        for i in range(len(self.data)-self.window_size):
+            feature = self.data[i:i+self.window_size].values.tolist()
+            target = self.data[i+1:i+self.window_size+1].values.tolist()
+            self.X.append(feature)
+            self.y.append(target)
+            
+
+    def return_X_y(self):
+        return torch.tensor(self.X), torch.tensor(self.y)
+
 
     def __len__(self):
-        return len(self.data)
+        return len(self.data) - self.window_size
+
 
     def __getitem__(self, index):
-        assert index < (len(self.data) + self.window_size), f'Index {index} out of range for given window size {self.window_size}'
-        x = self.data[index:index+self.window_size].values.tolist()
-        y = self.data[index+1:index+self.window_size+1].values.tolist()
-        return torch.tensor(x, dtype=torch.float64), torch.tensor(y, dtype=torch.float64)
+        assert index + self.window_size < len(self.data), f'Index {index} out of range for given window size {self.window_size}'
+        x, y = self.X[index], self.y[index]
+        return torch.tensor(x), torch.tensor(y)
--- prognose/models/lstm.py
+++ prognose/models/lstm.py
@@ -5,13 +5,12 @@ class LSTM(nn.Module):
 
     def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(LSTM, self).__init__()
-        self.model = nn.Sequential(
-            nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True),
-            nn.Linear(hidden_size, output_size)
-        )
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        y_pred = self.model(x)
+        y_pred, _ = self.lstm(x)
+        y_pred = self.linear(y_pred)
         return y_pred
 
 
