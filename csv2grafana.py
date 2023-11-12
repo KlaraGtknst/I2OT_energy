2b54cd8d9f9a05cf44d3ebd3dd7ab3057a9fb23a
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-11-12 12:30:08 +0100

    feat: added random generation in seconds

--- csv2grafana.py
+++ csv2grafana.py
@@ -1,14 +1,22 @@
+from datetime import datetime
+import time
 import numpy as np
 import pandas as pd
 from pathlib import Path 
-np.random.seed(1)
+import matplotlib.pyplot as plt
 
-rows,cols = 100,1
-data = np.random.rand(rows,cols) # You can use other random functions to generate values with constraints
-tidx = pd.date_range('2019-01-01', periods=rows, freq='T') # freq='MS'set the frequency of date in months and start from day 1. You can use 'T' for minutes and so on
-data_frame = pd.DataFrame(data, columns=['Power'], index=tidx)
-print(data_frame)
+while True:
+    rows,cols = 1000,1   # (number of samples, number of columns)
+    data = np.random.rand(rows,cols)
+    tidx = pd.date_range(datetime.now(), periods=rows, freq='s')
+    data_frame = pd.DataFrame(data, columns=['Power'], index=tidx)
+    print(data_frame)
 
-filepath = Path('csv_exports/out.csv')  
-filepath.parent.mkdir(parents=True, exist_ok=True)  
-data_frame.to_csv(filepath)  
+    filepath = Path('csv_exports/out.csv')  
+    filepath.parent.mkdir(parents=True, exist_ok=True)  
+    data_frame.to_csv(filepath)  
+
+    # plt.plot(data_frame)
+    # plt.show()
+    time.sleep(1)
+
